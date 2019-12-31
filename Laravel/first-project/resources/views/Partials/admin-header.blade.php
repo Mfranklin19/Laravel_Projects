cf4b50e907d4f1a8892801db19259aed1227863f
Michael Franklin <franklm@miamioh.edu>
2019-12-30 19:13:16 -0600

    Added routing and views to make it a multi-page application.

--- Laravel/first-project/resources/views/Partials/admin-header.blade.php
+++ Laravel/first-project/resources/views/Partials/admin-header.blade.php
@@ -3,11 +3,8 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="{{route('blog.index')}}">Home <span class="sr-only">(current)</span></a>
-            </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{route('other.about')}}">About</a>
+                <a class="nav-link" href="{{route('blog.post', ['id' => 1])}}">Posts</a>
             </li>
         </ul>
     </div>
---
+++ Laravel/first-project/resources/views/blog/post.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <p class="quote">Learning Laravel</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <p>
+                This is the content of a blog post!
+            </p>
+        </div>
+    </div>
+@endsection
